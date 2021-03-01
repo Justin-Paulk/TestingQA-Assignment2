a89346fad5d9b985db3ab11552c75c81cbc659cf
Justin Paulk <55258185+CognizantPotato@users.noreply.github.com>
2021-03-01 17:28:08 -0600

    Updating to feature the most recent tests.

--- main.py
+++ main.py
@@ -1,16 +1,104 @@
-# This is a sample Python script.
+def bmi_calc(feet, inches, weight):
+    kg = pound_to_kg(weight)
+    inch_height = height_to_inches(feet, inches)
+    meters = inches_to_meters(inch_height)
+    bmi = kg / (meters * meters)
+    bmi = round(bmi, 1)
+    if bmi < 18.5:
+        final_bmi = ['Underweight', bmi]
+    elif bmi <= 24.9:
+        final_bmi = ['Normal Weight', bmi]
+    elif bmi <= 29.9:
+        final_bmi = ['Overweight', bmi]
+    else:
+        final_bmi = ['Obese', bmi]
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    return final_bmi
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def pound_to_kg(pounds):
+    kg = pounds * 0.45
+    return kg
 
 
-# Press the green button in the gutter to run the script.
+def height_to_inches(feet, inches):
+    inches = (feet * 12) + inches
+    return inches
+
+
+def inches_to_meters(inches):
+    meters = inches * 0.025
+    return meters
+
+
+def retirement_calc(age, salary, prcSaved, goal):
+    yearly_saved = saved_per_year(salary, prcSaved)
+    retire_age = years_to_goal(yearly_saved, goal) + age
+    return retire_age
+
+
+def saved_per_year(salary, prcSaved):
+    saved = (salary * prcSaved) + ((salary * prcSaved) * 0.35)
+    return saved
+
+
+def years_to_goal(yearly_saved, goal):
+    return goal / yearly_saved
+
+
+def check_init_input(user_input):
+    if user_input == '1' or user_input == '2' or user_input == '3':
+        return True
+    else:
+        return False
+
+
+def handle_bmi_input(user_input):
+    newVal = user_input.split()
+    result = bmi_calc(int(newVal[0]), int(newVal[1]), float(newVal[2]))
+    return result
+
+
+def handle_retirement_input(user_input):
+    newVal = user_input.split()
+    if float(newVal[2]) > 1.0:
+        raise ValueError('Percent Cannot be Over 1.0')
+    result = retirement_calc(float(newVal[0]), float(newVal[1]), float(newVal[2]), float(newVal[3]))
+    return result
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    run = True
+    while run:
+        print("Please enter the integer value beside the function you wish to use")
+        print("1. BMI Calculator")
+        print("2. Retirement Savings Calculator")
+        print('3. Exit')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        val = input()
+
+        if not check_init_input(val):
+            print("Invalid Input. Please enter a valid integer")
+
+
+        if val == '1':
+            print("Please enter the height in feet and inches followed by the weight in pounds")
+            print("Ex: 5 8 150")
+            val = input()
+
+            result = handle_bmi_input(val)
+
+            print(result[0], ": ", result[1])
+        elif val == '2':
+            print("Please enter the current age, salary, percent saved, and retirement goal")
+            print("Ex: 21 100000 0.15 500000")
+            val = input()
+            result = round(handle_retirement_input(val), 1)
+
+            print("Goal will be met at ", result, " years old.")
+
+            if result >= 100:
+                print("The savings goal will not be met.")
+
+        elif val == '3':
+            print("Closing software")
+            run = False
---
+++ testing_main.py
@@ -0,0 +1,86 @@
+import unittest
+from main import *
+
+
+class BMITest(unittest.TestCase):
+    def test_bmi_calc(self):
+        real = bmi_calc(5, 3, 125)
+        assumed = ['Normal Weight', 22.7]
+        self.assertEqual(assumed, real)
+
+    def test_pound_to_kg(self):
+        kg = pound_to_kg(1)
+        self.assertEqual(kg, 0.45)
+
+    def test_height_to_inches(self):
+        inches = height_to_inches(1, 1)
+        self.assertEqual(inches, 13)
+
+    def test_inches_to_meters(self):
+        meters = inches_to_meters(1)
+        self.assertEqual(meters, 0.025)
+
+
+class RetirementTest(unittest.TestCase):
+    def test_retirement_calc(self):
+        retire_age = retirement_calc(21, 40000, 0.25, 500000)
+        self.assertAlmostEqual(retire_age, 58.00, 1)
+
+    def test_saved_per_year(self):
+        saved = saved_per_year(40000, .25)
+        self.assertEqual(saved, 13500)
+
+    def test_years_to_goal(self):
+        years = years_to_goal(100000, 1000000)
+        self.assertEqual(years, 10)
+
+
+class InterfaceTest(unittest.TestCase):
+    def test_check_init_input(self):
+        real = check_init_input('1')
+        self.assertEqual(real, True)
+
+    def test_bad_init_input(self):
+        real = check_init_input('one')
+        self.assertEqual(real, False)
+
+    def test_handle_bmi_input(self):
+        real = handle_bmi_input('5 3 125')
+        assumed = ['Normal Weight', 22.7]
+        self.assertEqual(assumed, real)
+
+    def test_string_bmi_input(self):
+        with self.assertRaises(ValueError) as real:
+            handle_bmi_input('five 3 125')
+        self.assertEqual(real.exception.__class__, ValueError)
+
+    def test_indexerr_bmi_input(self):
+        with self.assertRaises(IndexError) as real:
+            handle_bmi_input('5')
+        self.assertEqual(real.exception.__class__, IndexError)
+
+    def test_float_bmi_input(self):
+        with self.assertRaises(ValueError) as real:
+            handle_bmi_input('5.4 3 125')
+        self.assertEqual(real.exception.__class__, ValueError)
+
+    def test_handle_retirement_input(self):
+        real = handle_retirement_input('21 40000 0.25 500000')
+        assumed = 58.0
+        self.assertAlmostEqual(assumed, real, 1)
+
+    def test_string_retirement_input(self):
+        with self.assertRaises(ValueError) as real:
+            handle_retirement_input('twenty-one 40000 0.25 500000')
+        self.assertEqual(real.exception.__class__, ValueError)
+
+    def test_indexerr_retirement_input(self):
+        with self.assertRaises(IndexError) as real:
+            handle_retirement_input('21')
+        self.assertEqual(real.exception.__class__, IndexError)
+
+    def test_over_prc_retirement(self):
+        with self.assertRaises(ValueError) as real:
+            handle_retirement_input('21 40000 1.1 50000')
+        self.assertEqual(real.exception.__class__, ValueError)
+
